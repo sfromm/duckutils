dc72d9f16e2abb93bdaf28299027fbb8c0a04016
Stephen Fromm <stephenf@nero.net>
2013-05-16 16:41:40 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+from duckutils import __version__, __author__, __name__
+from distutils.core import setup
+
+setup(name=__name__,
+      version=__version__,
+      author=__author__,
+      author_email='sfromm@gmail.com',
+      url='https://github.com/sfromm/duckutils',
+      license='GPLv3',
+      package_dir={'duckutils': 'duckutils'},
+      packages=['duckutils']
+      )
